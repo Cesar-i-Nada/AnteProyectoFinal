7bc1d66db718c5d647810c5a77f7ea1a2fabb7e9
Cesar-i-Nada <cesar.alvarado.vega@gmail.com>
2025-06-12 11:59:48 -0600

    PROFILES LINK

--- FE/front_show_scout/src/Components/AccessLikeReg.jsx
+++ FE/front_show_scout/src/Components/AccessLikeReg.jsx
@@ -15,17 +15,17 @@ function AccessLikeReg() {
         <div className="flex-container">
           <div>
             <img src="src/assets/img/catUser.png" alt="Categoría de usuario" width={300} height={300}/>
-            <button  className='abcR'><Link to ="/CreateProfileA" className="myButtonL">Artista</Link></button>
+            <button  className='abcR'><Link to ="/ProfileUForm" className="myButtonL">Artista</Link></button>
 
           </div><br />
           <div>
             <img src="src/assets/img/catComp.png" alt="Categoría de compañía" width={300} height={300}/>
-            <button  className='abcR'><Link to ="/CreateProfileC" className="myButtonL">Compañía</Link></button>
+            <button  className='abcR'><Link to ="/ProfileCForm" className="myButtonL">Compañía</Link></button>
           </div><br />
           
           <div>
             <img src="src/assets/img/catOrg.png" alt="Categoría de Organización" width={300} height={300}/>
-            <button  className='abcR'><Link to ="/CreateProfileO" className="myButtonL">Organización</Link></button>
+            <button  className='abcR'><Link to ="/ProfileOForm" className="myButtonL">Organización</Link></button>
 
           </div>
           
