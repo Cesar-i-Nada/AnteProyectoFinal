77b05cad0530395efc199b237f542ecc99faf0bd
Cesar-i-Nada <cesar.alvarado.vega@gmail.com>
2025-06-14 15:37:03 -0600

    PROFILES ACO

--- FE/front_show_scout/src/Components/CreateProfileO.jsx
+++ FE/front_show_scout/src/Components/CreateProfileO.jsx
@@ -9,14 +9,14 @@ function CreateProfileO() {
   const [organization_name,  SetOrganizationName] = useState("")
   const [organization_funding, SetOrganizationFunding] = useState("")
   const [organization_phone, SetOrganizationPhone] = useState("")
-  const [organization_email, SetOrganiationEmail] = useState("")
-  const [company_password,  SetCompanyPassword] = useState("")
-  const [company_country, SetCompanyCountry] = useState("")
-  const [company_address, SetCompanyAddress] = useState("")
-  const [company_type_profile, SetCompanyTypeProfile] = useState("")
-  const [company_website, SetCompanyWebsite] = useState("")
-  const [company_social_media, SetCompanySocialMedia] = useState("")
-  const [companies, SetCompanies] = useState([])
+  const [organization_email, SetOrganizationEmail] = useState("")
+  const [organization_password,  SetOrganizationPassword] = useState("")
+  const [organization_country, SetOrganizationCountry] = useState("")
+  const [organization_address, SetOrganizationAddress] = useState("")
+  const [organization_type_profile, SetOrganizationTypeProfile] = useState("")
+  const [organization_website, SetOrganizationWebsite] = useState("")
+  const [organization_social_media, SetOrganizationSocialMedia] = useState("")
+  const [organizations, SetOrganizations] = useState([])
   
   const navigate = useNavigate()
 
@@ -24,8 +24,8 @@ function CreateProfileO() {
 
     async function fetchDataCompanies() {
 
-      const datos = await fetchCompanies.getCompanies()
-      SetCompanies(datos)
+      const datos = await fetchOrganizations.getOrganizations()
+      SetOrganizations(datos)
     };
 
     fetchDataCompanies();
@@ -33,25 +33,25 @@ function CreateProfileO() {
 
 function Create() { 
 
-  const registered = companies.filter(company => company.companyName === company_name && company.companyPassword === company_password)
+  const registered = organizations.filter(organization => organization.organizationName === organization_name && organization.organizationPassword === organization_password)
     
     console.log(registered);
     
     if (registered.length === 0) {
-      console.log('Compañía no registrada');
+      console.log('Organización no registrada');
       return;
     } 
     
-    const compañíaEncontrada = registered [0]; 
+    const organizacionEncontrada = registered [0]; 
     
-      //valida si el  perfil ya se realizó
-    if (compañíaEncontrada.perfilCreado) {
-      console.log('La compañía está registrada, por lo que puede ingresar');
-      localStorage.setItem("compañía", JSON.stringify(compañíaEncontrada));
+      //valida si el perfil ya se realizó
+    if (organizacionEncontrada.perfilCreado) {
+      console.log('La organización está registrada, por lo que puede ingresar');
+      localStorage.setItem("organización", JSON.stringify(organizacionEncontrada));
       navigate('/SynchroMap');
     }else{
-      console.log('La compañía aún no ha creado un perfil');  
-      localStorage.setItem("compañía",JSON.stringify(compañíaEncontrada))
+      console.log('La organización aún no ha creado un perfil');  
+      localStorage.setItem("organización",JSON.stringify(organizacionEncontrada))
       navigate('/CreateProfileC')
     }
    }
@@ -60,25 +60,25 @@ return (
 
 <div>
     
-    <div className="containerC">
+    <div className="containerO">
         <div>
             <img className='Cabritas' src="src/assets/img/CabritasClr.gif"/>   
         </div>
 
         <div>  
-          <div className='espCreateU'>
-            <p className='textCreateU'>Cree su perfil</p><br />
-            <input className='inpC' value={company_name} onChange={(e)=>SetCompanyName(e.target.value)} type="text" placeholder='Alias'/>
-            <input className='inpC' value={company_funding} onChange={(e)=>SetCompanyFunding(e.target.value)} type="text" placeholder='Fecha de fundación'/>
-            <input className='inpC' value={company_phone} onChange={(e)=>SetCompanyPhone(e.target.value)} type="text" placeholder='Teléfono'/>
-            <input className='inpC' value={company_email} onChange={(e)=>SetCompanyEmail(e.target.value)} type="text" placeholder='Correo'/>
-            <input className='inpC' value={company_password} onChange={(e)=>SetCompanyPassword(e.target.value)} type="text" placeholder='Contraseña'/>
-            <input className='inpC' value={company_country} onChange={(e)=>SetCompanyCountry(e.target.value)} type="text" placeholder='País' />
-            <input className='inpC' value={company_address} onChange={(e)=>SetCompanyAddress(e.target.value)} type="text" placeholder='Dirección'/>
-            <input className='inpC' value={company_type_profile} onChange={(e)=>SetCompanyTypeProfile(e.target.value)} type="text" placeholder='Tipo de perfil'/>
-            <input className='inpC' value={company_website} onChange={(e)=>SetCompanyWebsite(e.target.value)} type="text" placeholder='Sitio Web'/>
-            <input className='inpC' value={company_social_media} onChange={(e)=>SetCompanySocialMedia(e.target.value)} type="text" placeholder='Redes sociales'/>
-            <button onClick={Create} className='btnIniciarC'>Crear</button><br /><br />
+          <div className='espCreateO'>
+            <p className='textCreateO'>Cree su perfil</p><br />
+            <input className='inpO' value={organization_name} onChange={(e)=>SetOrganizationName(e.target.value)} type="text" placeholder='Alias'/>
+            <input className='inpO' value={organization_funding} onChange={(e)=>SetOrganizationFunding(e.target.value)} type="text" placeholder='Fecha de fundación'/>
+            <input className='inpO' value={organization_phone} onChange={(e)=>SetOrganizationPhone(e.target.value)} type="text" placeholder='Teléfono'/>
+            <input className='inpO' value={organization_email} onChange={(e)=>SetOrganizationEmail(e.target.value)} type="text" placeholder='Correo'/>
+            <input className='inpO' value={organization_password} onChange={(e)=>SetOrganizationPassword(e.target.value)} type="text" placeholder='Contraseña'/>
+            <input className='inpO' value={organization_country} onChange={(e)=>SetOrganizationCountry(e.target.value)} type="text" placeholder='País' />
+            <input className='inpO' value={organization_address} onChange={(e)=>SetOrganizationAddress(e.target.value)} type="text" placeholder='Dirección'/>
+            <input className='inpO' value={organization_type_profile} onChange={(e)=>SetOrganizationTypeProfile(e.target.value)} type="text" placeholder='Tipo de perfil'/>
+            <input className='inpO' value={organization_website} onChange={(e)=>SetOrganizationWebsite(e.target.value)} type="text" placeholder='Sitio Web'/>
+            <input className='inpO' value={organization_social_media} onChange={(e)=>SetOrganizationSocialMedia(e.target.value)} type="text" placeholder='Redes sociales'/>
+            <button onClick={Create} className='btnIniciarO'>Crear</button><br /><br />
           </div>
         </div>
 
@@ -89,4 +89,4 @@ return (
     );
   };
   
-export default CreateProfileC
+export default CreateProfileO
