8a679eae6fe672d35295a35e57421a952c071fad
Cesar-i-Nada <cesar.alvarado.vega@gmail.com>
2025-06-15 21:38:34 -0600

    register button

--- FE/front_show_scout/src/Components/FormRegister.jsx
+++ FE/front_show_scout/src/Components/FormRegister.jsx
@@ -8,15 +8,15 @@ import fetchUsers from '../Services/fetchUsers'
 function FormRegister() {
 
 const [username,  SetUsername] = useState("")
-const [passwordUser, SetPasswordUser] = useState("")
-const [emailUser, SetEmailUser] = useState("")
+const [userPassword, SetUserPassword] = useState("")
+const [userEmail, SetUserEmail] = useState("")
 
 
  function CrearRegistro() {
     const objUsuario ={
         username: username,
-        user_password: passwordUser,
-        user_email: emailUser
+        user_password: userPassword,
+        user_email: userEmail
     }
 
    fetchUsers.postUsers(objUsuario)
@@ -26,14 +26,14 @@ const [emailUser, SetEmailUser] = useState("")
 return (
 <div>
 
-  <div className='container'>
+  <div className='containerR'>
 
     <div className='espRegister'>
-      <h1 className='titulo'>Registrese</h1>
-      <p className='titulo'>para comenzar su viaje en la creación de obras escénicas</p><br />
-      <input className='inp'  placeholder="Alias" required onChange={(e)=>SetUsername(e.target.value)} type="text"/><br /><br />   
-      <input className='inp'  placeholder="Correo electrónico" required onChange={(e)=>SetEmailUser(e.target.value)} type="email" /><br /><br />
-      <input className='inp'  placeholder="Contraseña" required onChange={(e)=>SetPasswordUser(e.target.value)} type="password"/><br /><br />
+      <h1 className='tituloR'>Registrese</h1>
+      <p className='tituloR'>para comenzar su viaje en la creación de obras escénicas</p><br />
+      <input className='inpR'  placeholder="Alias" required onChange={(e)=>SetUsername(e.target.value)} type="text"/><br /><br />   
+      <input className='inpR'  placeholder="Correo electrónico" required onChange={(e)=>SetUserEmail(e.target.value)} type="email" /><br /><br />
+      <input className='inpR'  placeholder="Contraseña" required onChange={(e)=>SetUserPassword(e.target.value)} type="password"/><br /><br />
       <button onClick={CrearRegistro} className='btnRegistrar'>Registrarse</button><br /><br />
       <Link to={"/"}>Ir a Login</Link>
     </div>
