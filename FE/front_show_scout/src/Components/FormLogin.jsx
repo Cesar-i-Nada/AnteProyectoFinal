df619522927512ae406b26ddbbd58b635c15fcd5
Cesar-i-Nada <cesar.alvarado.vega@gmail.com>
2025-06-15 21:27:51 -0600

    register login

--- FE/front_show_scout/src/Components/FormLogin.jsx
+++ FE/front_show_scout/src/Components/FormLogin.jsx
@@ -7,7 +7,7 @@ import Cabritas from '../assets/img/CabritasClr.gif'
 function FormLogin() {
 
   const [username,  SetUsername] = useState("")
-  const [passwordUser, SetPasswordUser] = useState("")
+  const [userPassword, SetUserPassword] = useState("")
   const [users, SetUsers] = useState([])
   
   const navigate = useNavigate()
@@ -25,7 +25,7 @@ function FormLogin() {
 
 function Start() { 
 
-    const registered = users.filter(user => user.user === username && user.password === passwordUser)
+    const registered = users.filter(user => user.user === username && user.password === userPassword)
     
     console.log(registered);
     
@@ -51,7 +51,7 @@ function Start() {
 return (
 <div>
     
-    <div className="container">
+    <div className="containerL">
         <div>
             <img className='Cabritas' src="src/assets/img/CabritasClr.gif"/>   
         </div>
@@ -59,24 +59,24 @@ return (
         <div>  
           <div className='espLogin'>
             <p className='textLogin'>Lleve su producción <br /> por buen camino</p><br />
-            <input className='inp' placeholder="Alias" required onChange={(e)=>SetUsername(e.target.value)}type="text"/><br /><br />
-            <input className='inp' placeholder="Contraseña" required onChange={(e)=>SetPasswordUser(e.target.value)} type="password" /><br /><br />
-            <button onClick={Start} className='btnIniciar'>Iniciar</button><br /><br />
+            <input className='inpL' placeholder="Alias" required onChange={(e)=>SetUsername(e.target.value)}type="text"/><br /><br />
+            <input className='inpL' placeholder="Contraseña" required onChange={(e)=>SetUserPassword(e.target.value)} type="password" /><br /><br />
+            <button onClick={Start} className='btnIniciarL'>Iniciar</button><br /><br />
             <p>¿Ya tienes una cuenta? <Link to = "/Register">Ir a registrarse</Link></p>      
           </div>
         </div>
 
       </div>
 
-        <div className="textDiv">
+        <div className="textDivL">
           <h1>Herramientas para producir danza, teatro o circo.</h1>
         </div>
 
-      <div className="container">
+      <div className="containerL">
 
         <div>
-            <p className='segundoTexto'>Mejora los procesos de producción </p><br />
-            <p className='contSegundoTexto'>Esta herramienta es útil y práctica para tus creaciones de arte escénico</p>
+            <p className='segundoTextoL'>Mejora los procesos de producción </p><br />
+            <p className='contSegundoTextoL'>Esta herramienta es útil y práctica para tus creaciones de arte escénico</p>
         </div>
 
         <div>
