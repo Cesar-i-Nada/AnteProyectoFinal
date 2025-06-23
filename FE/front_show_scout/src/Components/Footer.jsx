323701ffedf8964576141462ad2437abca0502a1
Cesar-i-Nada <cesar.alvarado.vega@gmail.com>
2025-06-23 11:24:44 -0600

    footer margin

--- FE/front_show_scout/src/Components/Footer.jsx
+++ FE/front_show_scout/src/Components/Footer.jsx
@@ -11,8 +11,8 @@ function Footer() {
 
       <div className="flex-containerF">
         <div>
-            <ul className='listaFooter'>
-              <li className='titleFooterH'><b>Sobre nosotros</b></li><br />
+            <li className='titleFooterH'><b>Sobre nosotros</b></li><br />
+            <ul>
               <li><Link to={"/AllCourses"} className='titleFooter'>Cursos</Link></li>
               <li><Link to={"/MissionPage"} className='titleFooter'>Misión</Link></li>
               <li><Link to={"/ManualPage"} className='titleFooter'>Manual</Link></li>
@@ -25,8 +25,8 @@ function Footer() {
         </div>
 
         <div>
-            <ul className='listaFooter'>
-              <li className='titleFooterH'><b>Productos</b></li><br />
+            <li className='titleFooterH'><b>Productos</b></li><br />
+            <ul>
               <li><Link to={"/ShowScoutApp"} className='titleFooter'>ShowScout App</Link></li>
               <li><Link to={"/AppParaOrgs"} className='titleFooter'>ShowScout para organizaciones</Link></li>
               <li><Link to={"/PodcastChannel"} className='titleFooter'>Podcast</Link></li>
@@ -34,23 +34,23 @@ function Footer() {
         </div>
 
         <div>
-            <ul className='listaFooter'>
-              <li className='titleFooterH'><b>Apps</b></li><br />
+            <li className='titleFooterH'><b>Apps</b></li><br />
+            <ul>
               <li><Link to={"/DownloadApps"} className='titleFooter'>ShowScout para celular</Link></li>
             </ul>
         </div>
 
         <div>
-            <ul className='listaFooter'>
-              <li className='titleFooterH'><b>Ayuda y soporte</b></li><br />
+            <li className='titleFooterH'><b>Ayuda y soporte</b></li><br />
+            <ul>
               <li><Link to={"/SupportApp"} className='titleFooter'>ShowScout App</Link></li>
               <li><Link to={"/SupportAllCourses"} className='titleFooter'>Cursos</Link></li>
             </ul>
         </div>
 
         <div>
-            <ul className='listaFooter'>
-              <li className='titleFooterH'><b>Términos y privacidad</b></li><br />
+            <li className='titleFooterH'><b>Términos y privacidad</b></li><br />
+            <ul>
               <li><Link to={"/NormasPage"} className='titleFooter'>Normas</Link></li>
               <li><Link to={"/TerminosPage"} className='titleFooter'>Términos</Link></li>
               <li><Link to={"/PrivacidadPage"} className='titleFooter'>Privacidad</Link></li>
@@ -58,8 +58,8 @@ function Footer() {
         </div>
 
         <div>
-            <ul className='listaFooter'>
-              <li className='titleFooterH'><b>Social</b></li><br />
+            <li className='titleFooterH'><b>Social</b></li><br />
+            <ul>
               <li><Link to={"/BlogPage"} className='titleFooter'>Blog</Link></li>
               <li><Link to={"/InstagramPage"} className='titleFooter'>Instagram</Link></li>
               <li><Link to={"/YouTubeChannelPage"} className='titleFooter'>Canal de YouTube</Link></li>
