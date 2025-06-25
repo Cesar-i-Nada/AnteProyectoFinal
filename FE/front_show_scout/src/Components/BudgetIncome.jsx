dfb6b96c2907384f8b5cdd4c6b626906c39ee54d
Cesar-i-Nada <cesar.alvarado.vega@gmail.com>
2025-06-25 15:33:44 -0600

    avance de budget Income

--- FE/front_show_scout/src/Components/BudgetIncome.jsx
+++ FE/front_show_scout/src/Components/BudgetIncome.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react'
 import '../Styles/BudgetIncome.css'
 import fetchBudgets from '../Services/fetchBudgets'
 
+
+
 function CreateIncomes() {
 
   const [budget_income_pieces_name,  SetBudgetIncomePiecesName] = useState("")
@@ -48,33 +50,51 @@ function CreateIncomes() {
 
 return (
     <div>
-    
-    <div className="containerB">
-        <div>
-            <img className='Cabritas' src="src/assets/img/CabritasClr.gif"/>   
-        </div>
 
-        <div>  
-            <p className='textCreateB'>Complete la información de los ingresos de la pieza</p><br />
-            <div className='espCreateB'>
-                <label className='labelB'>Nombre de la pieza</label>
-                <input className='inpB' value={budget_income_pieces_name} onChange={(e)=>SetBudgetIncomePiecesName(e.target.value)} type="text" placeholder='Ingrese el nombre'/>
-                <br />
-                <label className='labelB'>Año en curso</label>
-                <input className='inpB' value={budget_income_pieces_year} onChange={(e)=>SetBudgetIncomePiecesYear(e.target.value)} type="text" placeholder='Ingrese el año'/>
-                <br />
-                <label className='labelB'>Venta de tiquetes</label>
-                <input className='inpB' value={budget_income_pieces_ticket_sales} onChange={(e)=>SetBudgetIncomePiecesTicketSales(e.target.value)} type="text" placeholder='Ingrese cantidad'/>
-                <br />
-                <label className='labelB'>Patrocinio</label>
-                <input className='inpB' value={budget_income_pieces_sponsorships} onChange={(e)=>SetBudgetIncomePiecesSponsorships(e.target.value)} type="text" placeholder='Ingrese cantidad'/>
+        <div className="containerB">
+
+            <div className='espBInc1'>
+                <img className='Cabritas' src="src/assets/img/CabritasClr.gif"/>   
+            </div>
+
+            <div>
+                <p className='textBInc'>Complete la información de los ingresos de la pieza</p><br />
+                <div className='espCreateB'>
+                    <label className='labelBInc'>Nombre de la pieza</label>
+                    <input className='inpLabBInc' value={budget_income_pieces_name} onChange={(e)=>SetBudgetIncomePiecesName(e.target.value)} type="text" placeholder='Ingrese el nombre'/>
                 <br /><br />
-                <button type='button' onClick={handleSubmit} className='btnIniciarB'>Cargar ingresos</button><br /><br />
-            <p className='totalIncomeB'>Total de ingresos</p>
-          </div>
+                    <label className='labelBInc'>Año en curso</label>
+                    <input className='inpLabBInc' value={budget_income_pieces_year} onChange={(e)=>SetBudgetIncomePiecesYear(e.target.value)} type="text" placeholder='Ingrese el año'/>
+                <br /><br />
+                <div>
+                    <table className='tableBInc'>
+                        <tr>
+                            <th className='thBInc'>Rubro</th>
+                            <th className='thBInc'>Cantidad</th>
+                            <th className='thBInc'>Monto</th>
+                            <th className='thBInc'>Subtotal</th>
+                        </tr>
+                        <tr>
+                            <td className='tdBInc'>Venta de tiquetes</td>
+                            <td><input className='inpBInc' value={budget_income_pieces_ticket_sales} onChange={(e)=>SetBudgetIncomePiecesTicketSales(e.target.value)} type="cant tiq" placeholder='Ingrese cantidad'/></td>
+                            <td><input className='inpBInc' type="mont $ tiq" placeholder='Ingrese monto' /></td>
+                            <td><input className='inpBInc' type="subtot tiq" /></td>
+                        </tr>
+                        <tr>
+                            <td className='tdBInc'>Patrocinios</td>
+                            <td><input className='inpBInc' value={budget_income_pieces_sponsorships} onChange={(e)=>SetBudgetIncomePiecesSponsorships(e.target.value)} type="cant pat" placeholder='Ingrese cantidad'/></td>
+                            <td><input className='inpBInc' type="mont $ pat" placeholder='Ingrese monto' /></td>                          
+                            <td><input className='inpBInc' type="subtot pat" /></td>
+                        </tr>
+                    </table><br />
+                    <button type='button' onClick={handleSubmit} className='btnBInc'>Cargar ingresos</button><br /><br />
+                    <p className='labelBInc'>Total de ingresos</p>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-    </div>
-)} 
+);
+};
 
-export default CreateIncomes
+export default CreateIncomes;
